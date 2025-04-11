3ed2a2694a5d981e8c05eab4725950dcadf327b8
GowthamAbi <gowtham2131ece@gmail.com>
2025-04-11 17:40:21 +0530

    server

---
+++ server.js
@@ -0,0 +1,4 @@
+const mongoose=require('mongoose');
+
+
+//mongoose.connect('mongodb+srv://gowthamabi1412:Gowtham2131%40@office.lrtep.mongodb.net/MediScripts').then(()=>{console.log("DB Connect Succesfully")}).catch((err)=>{console.log(err)})
