2481c5bb2973abf17488f5cf2578e236e4d39d4a
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-04-12 01:56:56 +0530

    db

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const mongoose=require('mongoose');
+const {MONGODB_URI, PORT } = require('./config/db');
+const app = require('./app');
 
+mongoose.connect(MONGODB_URI)
+.then(
+    ()=>{
+        console.log("DB CONNECTED SUCESSFULLY")
+        app.listen(PORT,()=>{
+            console.log(`Server run @ http://localhost:${PORT}`)
+        })
+    }
+
+)
+.catch((err)=>{console.log(err)})
 
