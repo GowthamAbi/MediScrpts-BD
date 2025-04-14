c928ba0816ef749db129f88914a6f4d0b971fda2
GowthamAbi <gowtham2131ece@gmail.com>
2025-04-14 15:10:22 +0530

    Db

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,13 @@ const { response } = require("../app");
 const User=require("../models/User")
 const jwt=require("jsonwebtoken")
 const bcrypt=require("bcrypt")
+const { JWT_SECRET } = require("../config/db");
+const { decrypt } = require("dotenv");
 
 const authController={
     register:async(req,res)=>{
         try{
+            //validate request body
             const{ name, email, password, phone, address } = req.body;
             //check if user already exists
             const user=await User.findOne({email})
@@ -14,9 +17,12 @@ const authController={
              {
                 return res.status(400).json({message:"User already exists"})
              }
+            //check if password is strong
+             const hashPassword=await bcrypt.hashSync(password,10)
+             //create new user
 
-             const newUser=new User({name, email, password, phone, address});
-
+             const newUser=new User({name, email, password:hashPassword, phone, address});
+             //save user to database
              await newUser.save();
              res.status(201).json({message:"User created successfully"});
 
@@ -28,7 +34,30 @@ const authController={
     },
     login:async(req,res)=>{
         try{
-            res.json({message:"Login Success"});
+          
+
+            //req to body
+            const {email,password}=req.body;
+            //check if user exists
+            const user=await User.findOne({email});
+            if(!user){
+                return res.status(400).json({message:"User not found"})
+            }
+
+            //check if password is correct
+            const isMatch=await bcrypt.compare(password,user.password)
+            console.log(user.password)
+            if(!isMatch){
+                return res.status(400).json({message:"Invalid credentials"})
+            }
+            //create token
+                const token=await jwt.sign({id:user._id},JWT_SECRET)
+
+                //set token in cookie
+                res.cookie("token",token,{httpOnly:true})
+
+
+                res.status(200).json({message:"Login Successfully"})
         }
         catch(err){
             console.log(err);
@@ -37,6 +66,7 @@ const authController={
     },
     logout:(req,res)=>{
         try{
+            res.clearCookie('cookies')
             res.json({message:"Logout Success"});
         }
         catch(err){
